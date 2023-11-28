1e6fb51dc253d544607422a512db1cc97a1fddcd
AkshayRajodiya <akshayrajodiya.ar@gmail.com>
2023-11-28 12:47:08 +0530

    assignment upload

---
+++ assignment5/assignFive/mydb.sql
@@ -0,0 +1,7 @@
+create table company (id integer, name varchar(100));
+
+insert into company (id, name) values (1, 'company1');
+insert into company (id, name) values (2, 'company2');
+insert into company (id, name) values (3, 'company3');
+insert into company (id, name) values (3, 'company4');
+insert into company (id, name) values (3, 'company5');
